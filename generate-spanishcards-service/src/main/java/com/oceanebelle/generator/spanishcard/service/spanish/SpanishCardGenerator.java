b31ab48a1c88f19358c9bbd88ab1367cd3f0431b
oceanebelle <oceanebelle@users.noreply.github.com>
2020-08-10 00:59:53 +0100

    minor improvements

--- generate-spanishcards-service/src/main/java/com/oceanebelle/generator/spanishcard/service/spanish/SpanishCardGenerator.java
+++ generate-spanishcards-service/src/main/java/com/oceanebelle/generator/spanishcard/service/spanish/SpanishCardGenerator.java
@@ -1,8 +1,6 @@
 package com.oceanebelle.generator.spanishcard.service.spanish;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public interface SpanishCardGenerator {
     public final static Integer HIGH = 1;
